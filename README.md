d5c40207e23bdb5be40348a098f13abacd759099
Anton Vorobyev <anton@vorobyev.co>
2018-02-12 20:12:01 +0000

    Add initial skeleton

---
+++ README.md
@@ -0,0 +1,15 @@
+# Starchitect Structures
+
+PHP library with Data Structures 
+
+## Installation
+
+Structures is available via Composer:
+
+```bash
+composer require starchitect/structures:dev-master
+```
+
+## License
+
+This project is licensed under the [Apache License 2.0](LICENSE).
---
+++ src/Object.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Starchitect\Structures;
+
+class Queue
+{
+    /**
+     * Creates a new Queue instance
+     */
+    public function __construct()
+    {
+        // TODO: Implement constructor body
+    }
+    
+	/**
+     * Enqueues object.
+     *
+     * @return void
+     */
+    public function enqueue($object)
+    {
+        // TODO: Implement method body
+		return;
+    }
+	
+}
